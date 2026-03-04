1e2d48b9dfbdf1b8be9b798db8aed1da0e2d78ed
Tanvighodekar30 <tanvighodekar@gmail.com>
2026-03-04 23:46:48 +0530

    my first program off c sharp

---
+++ login.cs
@@ -0,0 +1,54 @@
+﻿ 
+
+//class LoginForm
+//{
+//    static void Main()
+//    {
+//        string path = "C:\\Users\\Tanvi\\source\\repos\\ConsoleApp1\\registration.txt";
+
+//        Console.WriteLine("----- LOGIN FORM -----");
+
+//        Console.Write("Enter ID: ");
+//        string loginId = Console.ReadLine();
+
+//        Console.Write("Enter Email: ");
+//        string loginEmail = Console.ReadLine();
+
+//        bool found = false;
+
+//        if (File.Exists(path))
+//        {
+//            StreamReader sr = new StreamReader(path);
+//            string line;
+
+//            while ((line = sr.ReadLine()) != null)
+//            {
+//                string[] data = line.Split(',');
+
+//                string storedId = data[0];
+//                string storedEmail = data[4];
+
+//                if (storedId == loginId && storedEmail == loginEmail)
+//                {
+//                    found = true;
+//                    break;
+//                }
+//            }
+
+//            sr.Close();
+
+//            if (found)
+//            {
+//                Console.WriteLine("\nLogin Successfully");
+//            }
+//            else
+//            {
+//                Console.WriteLine("\nInvalid ID or Email ❌");
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("No registration records found.");
+//        }
+//    }
+//}
